4628a3f47e6d402c38f1e85188ee20e64c8f125a
JungAn2 <jungan1991@gmail.com>
2022-03-08 08:00:23 -0500

    Basic setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+  <div class="fixed-bottom justify-center">
+    
+    <q-btn round size="md" class="q-ma-md"><q-tooltip class="text-body2 z-max">Home Page</q-tooltip><img src="/favicon.ico"></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><q-tooltip class="text-body1 z-max">Play</q-tooltip></q-btn>
+    <q-btn round size="md" class="q-ma-md"><q-tooltip class="text-body2 z-maxs">Reset</q-tooltip></q-btn>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <br />
+  <h1>Click on the bubble</h1>
+  <div class="justify-center">
+    <q-btn round size="xl" class="q-ma-md" @click="change(T1)"><span v-if="T1">T</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md" @click="change(T2)"><span v-if="T2">I</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md" @click="fff()"><span v-if="T3">C</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T4">-</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T5">T</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T6">A</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T7">C</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T8">-</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T9">T</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T10">O</span></q-btn>
+    <q-btn round size="xl" class="q-ma-md"><span v-if="T11">E</span></q-btn>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const T1 = ref(false);
+const T2 = ref(false);
+const T3 = ref(false);
+const T4 = ref(false);
+const T5 = ref(false);
+const T6 = ref(false);
+const T7 = ref(false);
+const T8 = ref(false);
+const T9 = ref(false);
+const T10 = ref(false);
+const T11 = ref(false);
+
+const fff = ()=>{
+  console.log("asdf")
+}
+
+const change = (bubble) => {
+  console.log("Thsasdf")
+  bubble = true
+};
+</script>
+
+
+<style scoped>
+</style>
